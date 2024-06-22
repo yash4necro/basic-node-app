0edf19a15fb4bd7851be0867bb70620a039170d2
yash4necro <yash4necro@Yash.localdomain>
2024-06-23 00:20:23 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { add, sub, mod, prod } from "./math.js";
+
+console.log(add(3, 4));
+console.log(sub(3, 4));
+console.log(mod(3, 4));
+console.log(prod(3, 4));
---
+++ src/math.js
@@ -0,0 +1,7 @@
+export const add = (a, b) => a + b;
+
+export const sub = (a, b) => a - b;
+
+export const mod = (a, b) => a % b;
+
+export const prod = (a, b) => a * b;
